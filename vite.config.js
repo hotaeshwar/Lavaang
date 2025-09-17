5556c130d55e95365a967062cf3b6318df4edfd3
Himanshu <hotaeshwar@gmail.com>
2025-09-17 15:48:44 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  base: '/lavaang-dental-clinic/', // 👈 repo name
+  base: '/', // Changed from '/lavaang-dental-clinic/' to '/'
 })
