2fdcc668ee2497b7c5c617353493307aa4606022
Himanshu <hotaeshwar@gmail.com>
2025-09-17 15:28:13 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  base: '/',
+  base: '/lavaang-dental-clinic/', // 👈 repo name
 })
